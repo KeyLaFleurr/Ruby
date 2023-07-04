a8423e5b65efb689f5ddf173253484d745af6026
KeyLaFleurr <shinigamiseries6@gmail.com>
2023-07-04 08:52:58 +0700

    add file

---
+++ controllers/MetadataController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\Response;
+use yii\filters\VerbFilter;
+use app\models\LoginForm;
+use app\models\ContactForm;
+
+class MetadataController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+
+
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->renderPartial('index');
+    }
+
+}
---
+++ views/metadata/index.php
@@ -0,0 +1,103 @@
+<html>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+
+</head>
+
+<body>
+  <img src="/images/logo.png" width="150">
+  <table>
+    <tr>
+      <th> ㅤㅤ ㅤ ㅤㅤㅤㅤㅤ </th>
+    </tr>
+  </table>
+  <h1>
+    <center>METADATA STATISTIK</center>
+  </h1>
+  <h1>
+    <center><b>KEGIATAN</center></b>
+  </h1>
+  <table align="center" width="80%" rules="all" border="1" id="example">
+    <tr>
+      <th align="left" width="60%">Judul Kegiatan :</th>
+      <th align="right">Tahun : ㅤㅤ ㅤ</th>
+    </tr>
+    <tr>
+      <td colspan="2"><b>Kode Kegiatan (diisi oleh petugas) :</b></td>
+    </tr>
+    <tr>
+      <td colspan="1"><b>Cara Pengumpulan Data :</b>
+        <ul style="list-style-type: none;">
+          <li>Kompilasi Produk Administrasi</li>
+          <li>Cara lain sesuai dengan perkembangan TI</li>
+        </ul>
+      </td>
+
+      <td>
+        <ul style="list-style-type: none;"> <br>
+          <li>Pencacahan Lengkap</li>
+          <li>Survei</li>
+
+        </ul>
+      </td>
+    </tr>
+    <tr>
+
+      <td><b>Sektor Kegiatan : </b>
+        <ul style="list-style-type: none;">
+          <li>Pertanian dan perikanan</li>
+          <li>Demografi dan Kependudukan</li>
+          <li>Pembangunan</li>
+          <li>Proyeksi Ekonomi</li>
+          <li>Pendidikan dan Pelatihan</li>
+          <li>Lingkungan</li>
+          <li>Keuangan</li>
+          <li>Globalisasi</li>
+          <li>Kesehatan</li>
+          <li>Industri dan Jasa</li>
+          <li>Teknologi Informasi Dan Komunikasi</li>
+        </ul>
+      </td>
+      <td>
+        <ul style="list-style-type: none;"><br>
+          <li>Perdagangan Internasional dan Neraca Perdagangan</li>
+          <li>Ketenagakerjaan</li>
+          <li>Neraca Internasional</li>
+          <li>Indikator Ekonomi Bulanan</li>
+          <li>Produktivitas</li>
+          <li>Harga dan Paritas Harga Beli</li>
+          <li>Sektor Publik, Perpajakan, dan Regulasi Pasar</li>
+          <li>Perwilayahan dan Perkotaan</li>
+          <li>Ilmu Pengetahuan dan Hak Paten</li>
+          <li>Perlindungan Sosial dan Kesejahteraan Transportasi</li>
+        </ul>
+      </td>
+    </tr>
+    <tr>
+      <td colspan="2"><B>Jika Survei statistik sektoral, apakah mendapatkan rekomendasi kegiatan statistik dari BPS?</B>
+        <ul style="list-style-type: none;">
+          <li>Ya</li>
+          <li>Tidak</li>
+        </ul>
+        Jika "Ya", <b>Identitas Rekomendasi :
+        </b>.................................................................................
+
+      </td>
+
+    </tr>
+    <tr>
+      <th align="center" colspan="2">I. PENYELENGGARA</th>
+</tr>
+<tr>
+      <th align="left" colspan="2"><b>1.1 Instansi Penyelenggara:</b></th>
+</tr>
+  </table>
+</body>
+
+</html>
