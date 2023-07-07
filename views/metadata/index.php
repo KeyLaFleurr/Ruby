fc4ebce0d932b46f4c2e3e146950421ecccabfd0
kyoki <riskistwn0707@gmail.com>
2023-07-07 08:25:28 +0700

    halaman 82-83

--- views/metadata/index.php
+++ views/metadata/index.php
@@ -54,80 +54,432 @@
   <h1>
     <center><b>KEGIATAN</center></b>
   </h1>
-  <table align="center" width="80%" rules="all" border="1" id="example">
-    <tr>
-      <th class="judul" width="50%" height="80px">Judul Kegiatan :</th>
-      <th class="tahun">Tahun : ㅤㅤ ㅤ</th>
-    </tr>
-    <tr>
-      <td colspan="2" height="50px"><b>Kode Kegiatan (diisi oleh petugas) :</b><br></td>
-    </tr>
-    <tr>
-      <td colspan="1"><b>Cara Pengumpulan Data :</b>
-        <ul style="list-style-type: none;">
-        <li>Pencacahan Lengkap -1</li>
-          <li>Survei -2</li>
-        </ul>
-      </td>
+  
+  <table align="center" width="80%" rules="none" border="1">
+        <tr>
+            <th width="70%" align="left">Judul Kegiatan:</th>
+            <td width="10%"></td>
+        </tr>
 
-      <td>
-        <ul style="list-style-type: none;"> <br>
-        <li>Kompilasi Produk Administrasi -3</li>
-          <li>Cara lain sesuai dengan perkembangan TI -4</li>
-        </ul>
-      </td>
-    </tr>
-    <tr>
+        <tr>
+            <td></td>
+            <td>&emsp;</td>
+        </tr>
 
-      <td><b>Sektor Kegiatan : </b>
-        <ul style="list-style-type: none;">
-          <li>Pertanian dan perikanan -1</li>
-          <li>Demografi dan Kependudukan -2</li>
-          <li>Pembangunan -3</li>
-          <li>Proyeksi Ekonomi -4</li>
-          <li>Pendidikan dan Pelatihan -5</li>
-          <li>Lingkungan -6</li>
-          <li>Keuangan -7</li>
-          <li>Globalisasi -8</li>
-          <li>Kesehatan -9</li>
-          <li>Industri dan Jasa -10</li>
-          <li>Teknologi Informasi Dan Komunikasi -11</li>
-        </ul>
-      </td>
-      <td>
-        <ul style="list-style-type: none;"><br>
-          <li>Perdagangan Internasional dan Neraca Perdagangan</li>
-          <li>Ketenagakerjaan</li>
-          <li>Neraca Internasional</li>
-          <li>Indikator Ekonomi Bulanan</li>
-          <li>Produktivitas</li>
-          <li>Harga dan Paritas Harga Beli</li>
-          <li>Sektor Publik, Perpajakan, dan Regulasi Pasar</li>
-          <li>Perwilayahan dan Perkotaan</li>
-          <li>Ilmu Pengetahuan dan Hak Paten</li>
-          <li>Perlindungan Sosial dan Kesejahteraan Transportasi</li>
-        </ul>
-      </td>
-    </tr>
-    <tr>
-      <td colspan="2"><B>Jika Survei statistik sektoral, apakah mendapatkan rekomendasi kegiatan statistik dari BPS?</B>
-        <ul style="list-style-type: none;">
-          <li>Ya</li>
-          <li>Tidak</li>
-        </ul>
-        Jika "Ya", <b>Identitas Rekomendasi :
-        </b>.................................................................................
+        <tr>
+            <td></td>
+            <td>&emsp;</td>
+        </tr>
 
-      </td>
+        <tr>
+            <td></td>
+            <td>&emsp;</td>
+        </tr>
+    </table>
 
-    </tr>
+    <table align="center" width="80%" rules="none" border="1">
+        <tr>
+            <th align="left">Kode Kegiatan (diisi oleh petugas):</th>
+        </tr>
+
+        <tr>
+            <td></td>
+            <td>&emsp;</td>
+        </tr>
+
+        <tr>
+            <td></td>
+            <td>&emsp;</td>
+        </tr>
+    </table>
+
+    <table align="center" width="80%" rules="none" border="1">
+        <tr>
+            <th align="left" width="30%">Cara Pengumpulan Data:</th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>   
+        </tr>
+
+        <tr>
+            <td>&emsp;</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>   
+        </tr>
+
+        <tr>
+            <td width="30%">Pencerahan Lengkap</td>
+            <td width="5%">-1</td>
+            <td width="30%">Kompilasi Produk Administrasi</td>
+            <td width="5%">-3</td>
+            <td width="10%"></td>
+        </tr>
+
+        <tr>
+            <td>Survei</td>
+            <td>-2</td>
+            <td>Cara lain sesuai dengan perkembangan TI</td>
+            <td>-4</td>
+            <td></td>
+        </tr>
+        </table>
+
+    <table align="center" width="80%" rules="none" border="1">
+        <tr>
+            <th align="left" width="30%">Sektor Kegiatan:</th>
+            <td width="5%"></td>
+            <td width="30%"></td>
+            <td width="5%"></td>
+            <td width="10%"></td>
+        </tr>
+
+        <tr>
+            <td>&emsp;</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>   
+        </tr>
+
+        <tr>
+            <td width="30%">Pertanian dan Perikanan</td>
+            <td width="5%">-1</td>
+            <td width="30%">Perdagangan Internasional dan Neraca Perdagangan</td>
+            <td width="5%">-12</td>
+            <td width="10%"></td>
+        </tr>
+
+        <tr>
+            <td>Demografi dan Kependudukan</td>
+            <td>-2</td>
+            <td>Ketenagakerjaan</td>
+            <td>-13</td>
+            <td></td>
+        </tr>
+
+       <tr>
+            <td>Pembangunan</td>
+            <td>-3</td>
+            <td>Neraca Nasional</td>
+            <td>-14</td>
+            <td></td>
+        </tr> 
+
+        <tr>
+            <td>Proyeksi Ekonomi</td>
+            <td>-4</td>
+            <td>Indikator Ekonomi Bulanan</td>
+            <td>-15</td>
+            <td></td>
+        </tr>
+
+        <tr>
+            <td>Pendidikan dan Pelatihan</td>
+            <td>-5</td>
+            <td>Produktivitas</td>
+            <td>-16</td>
+            <td></td>
+        </tr>
+
+        <tr>
+            <td>Lingkungan</td>
+            <td>-6</td>
+            <td>Harga dan Paritas Daya Beli</td>
+            <td>-17</td>
+            <td></td>
+        </tr>
+
+        <tr>
+            <td>keuangan</td>
+            <td>-7</td>
+            <td>Sektor Publik, Perpajakan, dan Regulasi Pasar   </td>
+            <td>-18</td>
+            <td></td>
+        </tr>
+
+        <tr>
+            <td>Grobalisasi</td>
+            <td>-8</td>
+            <td>Perwilayahan dan Perkotaan</td>
+            <td>-19</td>
+            <td></td>
+        </tr>
+
+        <tr>
+            <td>kesehatan </td>
+            <td>-9</td>
+            <td>Ilmu Pengetahuan dan Hak Paten</td>
+            <td>-20</td>
+            <td></td>
+        </tr>
+
+        <tr>
+            <td>Industri dan Jasa</td>
+            <td>-10</td>
+            <td>Pelindungan Sosial dan Kesejahteraan</td>
+            <td>-21</td>
+            <td></td>
+        </tr>
+
+        <tr>
+            <td>Teknologi Informasi dan Komunikasi</td>
+            <td>-11</td>
+            <td>Transportasi</td>
+            <td>-22</td>
+            <td></td>
+        </tr>
+    </table>
+    <table width="80%" align="center" rules="none" border="1">
+        <tr>
+            <td><b>Jika survei sektoral, apakah mendapatkan rekomendasi kegiatan dari BPS?</b></td>
+        </tr>
+        <tr>
+            <td>Ya &emsp;&emsp; &emsp;&emsp;-1</td>
+
+        </tr>
+
+        <tr>
+            <td>Tidak &emsp;&emsp;&emsp;-2</td>
+        </tr>
+
+        <tr>
+            <td width="80%">Jika "Ya", <b>Identitas Rekomendasi:................................</b> </td>
+        </tr>
+    </table>
+</table>
+
+<table height="5%" align="center" width="80%" rules="none" border="1"  bgcolor="gainsboro">
+        <tr>
+            <td align="center"><b>I. PENYELENGGARA</b></td>
+        </tr>
+    </table>
+
+    <table align="center" width="80%" rules="none" border="1">
+        <tr>
+            <td><b>1.1.Instansi Penyelenggaraan:</b></td>
+        </tr>
+        <tr>
+            <td>&emsp;</td>
+        </tr>
+        <tr>
+            <td>&emsp;</td>
+        </tr>
+        <tr>
+            <td>&emsp;</td>
+        </tr>
+        <tr>
+            <td>&emsp;</td>
+        </tr>
+        <tr>
+            <td>&emsp;</td>
+        </tr>
+    </table> 
+    
+    <table align="center" width="80%" rules="none" border="1">
+        <tr>
+            <td><b>1.2.</b></td>
+            <td><b>Alamat Lengkap Instansi Penyelenggara:</b></td>
+            <td></td>
+        </tr>
+ 
+        <tr>
+            <td></td>
+            <td>&emsp;</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td width="3%"></td>
+            <td width="40%">Telpon &emsp; :</td>
+            <td>Faksimile &emsp; :</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>E-mail &emsp; :</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>&emsp;</td>
+            <td></td>
+        </tr>
+
+    </table> 
+    
+<!------------------------------------------------------------------------------------------------------------------------------------------------------->
+
+<table height="5%" align="center" width="80%" rules="none" border="1"  bgcolor="gainsboro">
     <tr>
-      <th align="center" colspan="2" class="mark"><b>I. PENYELENGGARA</b></th>
+        <td align="center"><b>II. PENANGGUNG JAWAB</b></td>
     </tr>
+</table>
+ 
+<table align="center" width="80%" rules="none" border="1">
     <tr>
-      <th align="left" colspan="2"><b>1.1 Instansi Penyelenggara:</b></th>
+        <td width="3%"><b>2.1.</b></td>
+        <td><b>Unit Eselon Penanggung Jawab</b></td>
     </tr>
-  </table>
+
+    <tr>
+        <td></td>
+        <td>&emsp;</td>
+    </tr>
+
+    <tr>
+        <td></td>
+        <td>Eselon 1 &emsp; :</td>
+    </tr>
+
+    <tr>
+        <td></td>
+        <td>Eselon 2 &emsp; :</td>
+    </tr>
+
+    </table>
+
+    <table align="center" width="80%" rules="none" border="1">
+    <tr>
+        <td width="3%"><b>2.2.</b></td>
+        <td width="40%"><b>Penanggung Jawab Teknis (setingkat Eselon 3)</b></td>
+        <td></td>
+    </tr>
+
+    <tr>
+        <td></td>
+        <td>&emsp;</td>
+    </tr>
+
+    <tr>
+        <td></td>
+        <td>Jabatan &emsp; :</td>
+        <td></td>
+    </tr>
+
+    <tr>
+        <td></td>
+        <td>Alamat &emsp; :</td>
+        <td></td>
+    </tr>
+
+    <tr>
+        <td></td>
+        <td>Telepon&emsp; :</td>
+        <td>Faksimile &emsp; :</td>
+    </tr>
+
+    <tr>
+        <td></td>
+        <td>E -mail  &emsp; :</td>
+        <td></td>
+    </tr>
+
+    <tr>
+        <td></td>
+        <td>&emsp;</td>
+        <td></td>
+    </tr>
+
+    <tr>
+        <td></td>
+        <td>&emsp;</td>
+        <td></td>
+    </tr>
+
+    </table>
+
+<!------------------------------------------------------------------------------------------------------------------------------------------------------->  
+
+<table height="5%" align="center" width="80%" rules="none" border="1"  bgcolor="gainsboro">
+    <tr>
+        <td align="center"><b>III. PERENCANAAN DAN PERSIAPAN</b></td>
+    </tr>
+</table>
+
+<table align="center" width="80%" rules="none" border="1">
+    <tr>
+        <td width="3%"><b>3.1.</b></td>
+        <td><b>Latar Belakang Kegiatan:</b></td>
+    </tr>
+
+    <tr>
+        <td></td>
+        <td>&emsp;</td>
+    </tr>
+
+    <tr>
+        <td></td>
+        <td>&emsp;</td>
+    </tr>
+
+    <tr>
+        <td></td>
+        <td>&emsp;</td>
+    </tr>
+
+    <tr>
+        <td></td>
+        <td>&emsp;</td>
+    </tr>
+
+    <tr>
+        <td></td>
+        <td>&emsp;</td>
+    </tr>
+
+    <tr>
+        <td></td>
+        <td>&emsp;</td>
+    </tr>
+
+    <tr>
+        <td></td>
+        <td>&emsp;</td>
+    </tr>
+
+    <tr>
+        <td></td>
+        <td>&emsp;</td>
+    </tr>
+
+    <table align="center" width="80%" rules="none" border="1">
+        <tr>
+            <td width="3%"><b>3.2.</b></td>
+            <td><b>Tujuan Kegiatan:</b></td>
+        </tr>
+    
+        <tr>
+            <td></td>
+            <td>&emsp;</td>
+        </tr>
+    
+        <tr>
+            <td></td>
+            <td>&emsp;</td>
+        </tr>
+
+        <tr>
+            <td></td>
+            <td>&emsp;</td>
+        </tr>
+
+        <tr>
+            <td></td>
+            <td>&emsp;</td>
+        </tr>
+    
+        <tr>
+            <td></td>
+            <td>&emsp;</td>
+        </tr>
+    
+        <tr>
+            <td></td>
+            <td>&emsp;</td>
+        </tr>
+
+  
 </body>
 
 </html>
